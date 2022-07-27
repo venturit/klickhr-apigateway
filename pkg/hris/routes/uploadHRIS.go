9b125860bc702f1e855d30e4e9a2f36f1077b5f9
Jhoan_Grajales <jhoan@venturit.com>
2022-07-27 08:10:11 -0500

    send file to hris microservice

--- pkg/hris/routes/uploadHRIS.go
+++ pkg/hris/routes/uploadHRIS.go
@@ -1,9 +1,7 @@
 package routes
 
 import (
-	"context"
-	"fmt"
-	"net/http"
+	"io"
 
 	"klickhr-apigateway/pkg/hris/pb"
 
@@ -11,33 +9,50 @@ import (
 )
 
 type UploadHRISRequestBody struct {
-	FileType       int    `json:"file_type"`
-	RunType        int    `json:"run_type"`
-	ImportType     int    `json:"import_type"`
-	OrganizationId int    `json:"organization_id"`
-	File           []byte `json:"file"`
+	FileType       int `form:"file_type"`
+	RunType        int `form:"run_type"`
+	ImportType     int `form:"import_type"`
+	OrganizationId int `form:"organization_id"`
 }
 
 func UploadHRIS(ctx *gin.Context, c pb.HRISServiceClient) {
 	body := UploadHRISRequestBody{}
-
-	if err := ctx.BindJSON(&body); err != nil {
-		ctx.AbortWithError(http.StatusBadRequest, err)
-		return
-	}
-
-	res, err := c.UploadHRIS(context.Background(), &pb.UploadHRISRequest{
-		FileType:       int64(body.FileType),
-		RunType:        int64(body.RunType),
-		ImportType:     int64(body.ImportType),
-		OrganizationId: int64(body.OrganizationId),
-		File:           body.File,
-	})
-
+	ctx.Bind(&body)
+	//get file
+	file, _ := ctx.FormFile("file")
+	// Maximum 10Mb size per stream.
+	buf := make([]byte, 1024*10000)
+	stream, err := c.UploadHRIS(ctx)
 	if err != nil {
-		ctx.AbortWithError(http.StatusBadGateway, err)
 		return
 	}
-	fmt.Println("a")
+	fil, err := file.Open() //os.Open("./" + file.Filename)
+	if err != nil {
+		return
+	}
+	for {
+		num, err := fil.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return
+		}
+
+		if err := stream.Send(&pb.UploadHRISRequest{
+			FileType:       int64(body.FileType),
+			RunType:        int64(body.RunType),
+			ImportType:     int64(body.ImportType),
+			OrganizationId: int64(body.OrganizationId),
+			FileName:       file.Filename,
+			FileBytes:      buf[:num],
+		}); err != nil {
+			return
+		}
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		return
+	}
 	ctx.JSON(int(res.Status), &res)
 }
