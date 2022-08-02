217ee22c1c17d4fab4e50af0798740dcaa1c670e
NicolasArizaRiberos <nicolas.ri@venturit.com>
2022-08-02 09:45:01 -0500

    Create endpoints apigateway

--- pkg/auth/routes.go
+++ pkg/auth/routes.go
@@ -17,6 +17,12 @@ func RegisterRoutes(r *gin.Engine, c *config.Config) *ServiceClient {
 	routes := r.Group("/auth")
 	routes.POST("/register", svc.Register)
 	routes.POST("/login", svc.Login)
+	routes.POST("/forgot", svc.Forgot)
+	routes.PATCH("/recovery", svc.Recovery)
+	//routes.GET("/refresh", svc.Recovery)
+	//routes.GET("/verifyemail/:verificationCode", rc.authController.VerifyEmail)
+	//routes.POST("/forgotpassword", rc.authController.ForgotPassword)
+	//routes.PATCH("/resetpassword/:resetToken", rc.authController.ResetPassword)
 
 	return svc
 }
@@ -27,4 +33,12 @@ func (svc *ServiceClient) Register(ctx *gin.Context) {
 
 func (svc *ServiceClient) Login(ctx *gin.Context) {
 	routes.Login(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) Recovery(ctx *gin.Context) {
+	routes.Recovery(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) Forgot(ctx *gin.Context) {
+	routes.Recovery(ctx, svc.Client)
 }
